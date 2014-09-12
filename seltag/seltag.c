d1d79b36d3a4dacb2c2a175a00d25d25ffd94706
Robert Olsson <robert@herjulf.se>
2014-09-12 10:40:58 +0200

    seltag add option to adjust date/time. Add/subtract number of sec.

--- seltag/seltag.c
+++ seltag/seltag.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
 
-#define VERSION "0.9 121231"
+#define VERSION "0.91 140912"
 
 /*
  *  Copyright Robert Olsson <robert@herjulf.se>
@@ -29,6 +30,56 @@ struct tag {
 };
 struct tag t[40];
 unsigned int year, mon, day, hour, min, sec;
+struct timeval tv;
+
+/* mktime from linux kernel */
+
+/*
+ *  linux/kernel/time.c
+ *
+ *  Copyright (C) 1991, 1992  Linus Torvalds
+ *
+ *  This file contains the interface functions for the various
+ *  time related system calls: time, stime, gettimeofday, settimeofday,
+ *			       adjtime
+ */
+
+
+/* Converts Gregorian date to seconds since 1970-01-01 00:00:00.
+ * Assumes input in normal date format, i.e. 1980-12-31 23:59:59
+ * => year=1980, mon=12, day=31, hour=23, min=59, sec=59.
+ *
+ * [For the Julian calendar (which was used in Russia before 1917,
+ * Britain & colonies before 1752, anywhere else before 1582,
+ * and is still in use by some communities) leave out the
+ * -year/100+year/400 terms, and add 10.]
+ *
+ * This algorithm was first published by Gauss (I think).
+ *
+ * WARNING: this function will overflow on 2106-02-07 06:28:16 on
+ * machines were long is 32-bit! (However, as time_t is signed, we
+ * will already get problems at other places on 2038-01-19 03:14:08)
+ */
+
+unsigned long our_mktime(const unsigned int year0, const unsigned int mon0,
+       const unsigned int day, const unsigned int hour,
+       const unsigned int min, const unsigned int sec)
+{
+	unsigned int mon = mon0, year = year0;
+
+	/* 1..12 -> 11,12,1..10 */
+	if (0 >= (int) (mon -= 2)) {
+		mon += 12;	/* Puts Feb last since it has leap day */
+		year -= 1;
+	}
+
+	return ((((unsigned long)
+		  (year/4 - year/100 + year/400 + 367*mon/12 + day) +
+		  year*365 - 719499
+	    )*24 + hour /* now have hours */
+	  )*60 + min /* now have minutes */
+	)*60 + sec; /* finally seconds */
+}
 
 
 int time_parse(char *buf)
@@ -40,16 +91,36 @@ int time_parse(char *buf)
     return res;
 }
 
+void time_adjust(int offset)
+{
+  struct timeval *tvp;
+  struct tm tm;
+  char tbuf[40];
+
+  tvp = &tv;
+
+  tvp->tv_sec = our_mktime(year, mon, day, hour, min, sec);
+  tvp->tv_usec = 0;
+  tvp->tv_sec += offset;
+  gmtime_r(&tvp->tv_sec, &tm);
+
+  strftime(tbuf, sizeof(tbuf), "%Y-%m-%d %H:%M:%S", &tm);
+  printf("%s ", tbuf);
+}
 
 int main(int ac, char *av[]) 
 {
   char buf[BUFSIZ], buf1[BUFSIZ], *res;
   int i, j, k, cpy;
   char timebuf[40];
+  int timeadjust = 0;
+
   if (ac == 1 || strcmp(av[1], "-h") == 0) 
     usage();
 
   cpy = atoi(av[1]);
+
+  //timeadjust = -2*60*60;
 
   while ( fgets ( buf, BUFSIZ, stdin ) != NULL ) {
     res = strtok( buf, " " );
@@ -75,9 +146,12 @@ int main(int ac, char *av[])
     }
 
     time_parse(timebuf);
-    printf("%04u-%02u-%02u %02u:%02u:%02u ",
-    		      year, mon, day, hour, min, sec);
 
+    if(timeadjust) 
+      time_adjust(timeadjust);
+    else
+      printf("%04u-%02u-%02u %02u:%02u:%02u ",
+    		      year, mon, day, hour, min, sec);
 
     for(k = 2; k < ac; k++) 
       printf("%s ", t[k].buf);
